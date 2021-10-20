bca4d548d23ae776c3ded72be32eb126c33a513e
Waldri <waldri.so@gmail.com>
2021-10-20 21:28:20 +0100

    Adicionar arquivos de projeto.

---
+++ Cantina/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace Cantina
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCod = new System.Windows.Forms.TextBox();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.labTotal = new System.Windows.Forms.Label();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantina";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Código do Produto";
+            // 
+            // txtCod
+            // 
+            this.txtCod.Location = new System.Drawing.Point(16, 110);
+            this.txtCod.Name = "txtCod";
+            this.txtCod.Size = new System.Drawing.Size(116, 20);
+            this.txtCod.TabIndex = 2;
+            this.txtCod.TextChanged += new System.EventHandler(this.txtCod_TextChanged);
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(16, 151);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(116, 105);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBox.TabIndex = 3;
+            this.picBox.TabStop = false;
+            // 
+            // labTotal
+            // 
+            this.labTotal.AutoSize = true;
+            this.labTotal.Location = new System.Drawing.Point(170, 232);
+            this.labTotal.Name = "labTotal";
+            this.labTotal.Size = new System.Drawing.Size(58, 13);
+            this.labTotal.TabIndex = 4;
+            this.labTotal.Text = "Valor Total";
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.Location = new System.Drawing.Point(173, 110);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(175, 95);
+            this.lstBox.TabIndex = 5;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(273, 79);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Limpar";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 277);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.labTotal);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.txtCod);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.Label labTotal;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Cantina/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cantina
+{
+    public partial class Form1 : Form
+    {
+        string[] produtos = new string[5];
+        string[] codigo = new string[5];
+        double[] valor = new double[5];
+        double soma;
+        decimal[] numero = new decimal[50];
+
+        private void loadArray()
+        {
+            codigo[1] = "001";
+            codigo[2] = "002";
+            codigo[3] = "003";
+            codigo[4] = "004";
+
+            produtos[1] = "Pastel";
+            produtos[2] = "Suco";
+            produtos[3] = "Hot-dog";
+            produtos[4] = "Coxinha";
+
+            valor[1] = 5.50;
+            valor[2] = 3.60;
+            valor[3] = 6.75;
+            valor[4] = 4.25;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtCod_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCod.Text.Length == 3)
+            {
+                /*lstBox.Items.Add(txtCod.Text);
+                txtCod.Text = "";
+                txtCod.Focus();*/
+                int index = 0;
+                for (int prod = 1; prod < codigo.Length; prod++)
+                {
+                    if (txtCod.Text == codigo[prod]) {
+                        index = prod;
+                    }
+                }
+                if(index == 0)
+                {
+                    MessageBox.Show("Produto não encontrado", "Alert");
+                }
+                else
+                {
+                    lstBox.Items.Add(txtCod.Text + " -- " + produtos[index] + " R$ " + valor[index]);
+                    soma = soma + valor[index];
+                    labTotal.Text = ("Valor Total R$ " + soma);
+                    picBox.ImageLocation = @"../../images/"+ codigo[index] +".jpg";
+                    txtCod.Text = "";
+                    txtCod.Focus();
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadArray();
+            soma = 0;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Clear();
+            soma = 0;
+            labTotal.Text = "";
+            txtCod.Focus();
+        }
+    }
+}
